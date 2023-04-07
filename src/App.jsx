97d4fc89a9ac5d7ed83956c64dea472f6cd312cc
Bruno M <brunomoron56@gmail.com>
2023-04-07 00:13:33 -0300

    add animation with framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import CharacterList from "./components/CharacterList"
+import CharacterList from "./components/CharacterList";
+import { motion } from "framer-motion";
 
 function App() {
-  return <div className="bg-zinc-800 text-white ">
-    <h1 className="text-center text-4xl sm:text-5xl lg:text-7xl text-green-500 font-rick-morty pt-10 pl-4">Rick and Morty</h1>
-    <CharacterList />
-  </div>;
+  return (
+    <div className="bg-zinc-800 text-white ">
+      <div className="text-center">
+        <motion.h1
+          whileHover={{ color: "white", rotateZ: -10 }}
+          transition={{ duration: 0.3 }}
+          className="inline-block m-auto text-4xl sm:text-5xl lg:text-7xl text-green-500 font-rick-morty pt-10 pl-4"
+        >
+          Rick and Morty
+        </motion.h1>
+      </div>
+
+      <CharacterList />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,9 +1,20 @@
-function Character({character}) {
+import { motion } from "framer-motion";
+
+function Character({ character }) {
   return (
     <div className="p-5 group flex flex-col lg:text-xl">
       <h3 className="group-hover:font-rick-morty">{character.name}</h3>
-      <img className="object-cover rounded-full place-self-center w-[70%]" src={character.image} alt={character.name} />
-      <p className="text-right group-hover:font-rick-morty group-hover:text-green-500">{character.origin.name === 'unknown' ? '???' : character.origin.name}</p>
+      <motion.img
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 0.8 }}
+        transition={{ duration: 0.4  }}
+        className=" mt-2 object-cover rounded-full place-self-center w-[70%]"
+        src={character.image}
+        alt={character.name}
+      />
+      <p className="text-right group-hover:font-rick-morty group-hover:text-green-500">
+        {character.origin.name === "unknown" ? "???" : character.origin.name}
+      </p>
     </div>
   );
 }
